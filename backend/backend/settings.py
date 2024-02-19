6bdc3aaa2f4ebdd263114bb1871045539eb4c69c
Ritik <ritikraj2699@outlook.com>
2024-02-20 02:27:29 +0530

    minor changes

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -31,12 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "core",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    
 ]
 
 MIDDLEWARE = [
@@ -73,20 +75,29 @@ WSGI_APPLICATION = "backend.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "hackathon_db",
+#         "USER": "kgpian",
+#         "PASSWORD": "kgpian",
+#         "HOST": "localhost",
+#         "PORT": "5432",   
+#     }
+# }
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": "hackathon_db",
-        "USER": "kgpian",
-        "PASSWORD": "kgpian",
-        "HOST": "localhost",
-        "PORT": "5432",   
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
-
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
+
+AUTH_USER_MODEL = 'core.User'
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
