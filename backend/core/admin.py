c94bf4b91de9ba676527a59116156480efaed3b1
Ritik <ritikraj2699@outlook.com>
2024-02-25 02:46:45 +0530

    updated project

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import Participant, Manager
+# from .models import Participant, Manager
 
-admin.site.register(Participant)
-admin.site.register(Manager)
-# admin.site.register(Admin)
+# admin.site.register(Participant)
+# admin.site.register(Manager)
