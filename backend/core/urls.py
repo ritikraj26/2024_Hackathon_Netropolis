7c03d143b2293610d9b8d63a029bc030da9b5742
Ritik <ritikraj2699@outlook.com>
2024-02-28 00:21:35 +0530

    backend changes

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -7,8 +7,26 @@ urlpatterns = [
     path("login/manager", core.views.login_manager, name="login_manager"),
     path("signup/user", core.views.signup_user, name="signup_user"),
     path("login/user", core.views.login_user, name="login_user"),
+    
+    #locations
     path("add/location", core.views.add_location, name="add_location"),
-    # path("login/", core.views.loginView, name="login"),
-    # path("signup/user", core.views.signupUser, name=""),
-    # path("logout/", "core.views.logout"),
+    path("add/location_type", core.views.add_location_type, name="add_location_type"),
+
+    #category
+    path("add/category", core.views.add_category, name="add_category"),
+    path("get/categories", core.views.get_categories, name="get_categories"),
+    path("get/category/<uuid:pk>", core.views.get_category_by_uuid, name="get_category_by_uuid"),
+    
+    #tasks
+    path("create/task", core.views.create_task, name="create_task"),
+    path("get/tasks", core.views.get_tasks, name="get_tasks"),
+    path("get/task/location/<str:pk>", core.views.get_task_by_location, name="get_task_by_location"),
+    path("get/task/category", core.views.get_task_by_category, name="get_task_by_category"),
+
+    #quest
+    path("create/quest", core.views.create_quest, name="create_quest"),
+    path("get/quest/location/<str:pk>", core.views.get_quest_by_location, name="get_quest_by_location"),
+
+    
+
 ]
--- backend/core/views.py
+++ backend/core/views.py
@@ -186,11 +186,192 @@ def logout_user(request):
 
 
 @csrf_exempt
-def get_task(request):
+def add_location_type(request):
+    if request.method == 'POST':
+        request_data_str = request.body.decode('utf-8')
+        request_data = json.loads(request_data_str)
+
+        name = request_data.get('name')
+        loaction_type_uuid = uuid.uuid4()
+        location_type = Location_Type.objects.create(uuid=loaction_type_uuid,name=name)
+        return HttpResponse('Location Type created successfully')
+    else:
+        return HttpResponse('Create Location Type Page')
+
+
+    
+
+
+@csrf_exempt
+def add_category(request):
+    if request.method == 'POST':
+        request_data_str = request.body.decode('utf-8')
+        request_data = json.loads(request_data_str)
+
+        name = request_data.get('name')
+        category_uuid = uuid.uuid4()
+        category = Category.objects.create(uuid=category_uuid, name=name)
+        return HttpResponse('Category created successfully')
+    else:
+        return HttpResponse('Create Category Page')
+    
+
+@csrf_exempt
+def get_categories(request):
     if request.method == 'GET':
-        task = Task.objects.all()
-        serialized_task = Task_Serializer(task, many=True)
-        return JsonResponse(serialized_task.data, safe=False)
+        categories = Category.objects.all()
+        serialized_categories = Category_Serializer(categories, many=True)
+        return JsonResponse(serialized_categories.data, safe=False)
+    
+@csrf_exempt
+def get_category_by_uuid(request,pk):
+    if request.method == 'GET':
+        category = Category.objects.get(uuid=pk)
+        serialized_category = Category_Serializer(category)
+        return JsonResponse(serialized_category.data, safe=False)
+    
+
+
+@csrf_exempt
+def create_task(request):
+    if request.method == 'POST':
+        request_data_str = request.body.decode('utf-8')
+        request_data = json.loads(request_data_str)
+
+        name = request_data.get('name')
+        description = request_data.get('description')
+        
+        category = None
+        location=None
+        location_type=None
+
+        category_name = request_data.get('category_name')
+        try:
+            category = Category.objects.get(name=category_name)
+        except Category.DoesNotExist:
+            return HttpResponse('Category does not exist', status=400)
+        
+        location_name = request_data.get('location_name')
+        try:
+            location = Location.objects.get(name=location_name)
+        except:
+            return HttpResponse('Location does not exist', status=400)
+        
+        location_type_name = request_data.get('location_type_name')
+        try:
+            location_type = Location_Type.objects.get(name=location_type_name)
+        except:
+            return HttpResponse('Location Type does not exist', status=400)
+        
+        points = request_data.get('points')
+        duration = request_data.get('duration')
+        task_uuid = uuid.uuid4()
+
+        task = Task.objects.create(
+            uuid=task_uuid,
+            name=name,
+            description=description,
+            category=category,
+            location=location,
+            location_type=location_type,
+            points=points,
+            duration=duration,
+        )
+        return HttpResponse('Task created successfully')
+    else:
+        return HttpResponse('Create Task Page')
+    
+
+@csrf_exempt
+def get_tasks(request):
+    if request.method == 'GET':
+        tasks = Task.objects.all()
+        serialized_tasks = Task_Serializer(tasks, many=True)
+        return JsonResponse(serialized_tasks.data, safe=False)
+    
+
+@csrf_exempt
+def get_task_by_location(request,pk):
+    if request.method == 'GET':
+        location = Location.objects.get(name=pk)
+        tasks = Task.objects.filter(location=location.uuid)
+        serialized_tasks = Task_Serializer(tasks, many=True)
+        return JsonResponse(serialized_tasks.data, safe=False)
+    
+@csrf_exempt
+def get_task_by_category(request):
+    if request.method == 'GET':
+        request_data_str = request.body.decode('utf-8')
+        request_data = json.loads(request_data_str)
+        name = request_data.get('name')
+        category = Category.objects.get(name=name)
+        tasks = Task.objects.filter(category=category.uuid)
+        serialized_tasks = Task_Serializer(tasks, many=True)
+        return JsonResponse(serialized_tasks.data, safe=False)
+    
+
+@csrf_exempt
+def create_quest(request):
+    if request.method == 'POST':
+        request_data_str = request.body.decode('utf-8')
+        request_data = json.loads(request_data_str)
+
+        name = request_data.get('name')
+        description = request_data.get('description')
+        max_people = request_data.get('max_people')
+        location_name = request_data.get('location_name')
+        creator_uuid = request_data.get('creator_uuid')
+
+        location = Location.objects.get(name=location_name)
+        created_by = Participant.objects.get(uuid=creator_uuid)
+        quest_uuid = uuid.uuid4()
+
+        total_points=0
+        total_duration=0
+
+        tasks = request_data.get('tasks')
+
+        for task in tasks:
+            task_uuid = task.get('uuid')
+            task = Task.objects.get(uuid=task_uuid)
+            total_points += task.points
+            total_duration += task.duration
+
+        quest = Quest.objects.create(
+            uuid=quest_uuid,
+            name=name,
+            description=description,
+            max_people=max_people,
+            location=location,
+            created_by=created_by,
+            total_points=total_points,
+            total_duration=total_duration
+        )
+
+        print(quest)
+        for task in tasks:
+            task_uuid = task.get('uuid')
+            day_number = task.get('day_number')
+            task = Task.objects.get(uuid=task_uuid)
+            quest_task = Quest_Task.objects.create(
+                uuid=uuid.uuid4(),
+                quest=quest,
+                task=task,
+                is_completed=False,
+                day_number=day_number
+            )
+        return HttpResponse('Quest created successfully')
+    else:
+        return HttpResponse('Create Quest Page')
+    
+@csrf_exempt
+def get_quest_by_location(request,pk):
+    if request.method == 'GET':
+        location = Location.objects.get(name=pk)
+        quests = Quest.objects.filter(location=location.uuid)
+        serialized_quests = Quest_Serializer(quests, many=True)
+        return JsonResponse(serialized_quests.data, safe=False)
+    
 
 
 
