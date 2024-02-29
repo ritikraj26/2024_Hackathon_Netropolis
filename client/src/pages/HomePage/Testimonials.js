a6563b1e5fd60e93eb50493d41cf9481169767c7
Ritik <ritikraj2699@outlook.com>
2024-03-01 00:20:01 +0530

    minor changes

--- client/src/pages/HomePage/Testimonials.js
+++ client/src/pages/HomePage/Testimonials.js
@@ -67,7 +67,7 @@ const Testimonials = () => {
           />
           <div className="flex items-center divide-x-2 rtl:divide-x-reverse divide-gray-300 dark:divide-gray-700">
             <cite className="pe-3 font-medium text-gray-900 dark:text-white">
-              Netropopils LLC
+              Netropolis LLC
             </cite>
             <cite className="ps-3 text-sm text-gray-500 dark:text-gray-400">
               Japan
